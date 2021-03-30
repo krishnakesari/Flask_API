19b03554124a641fb80672d25a6381bdc96f854a
krishnakesari <bkesari@outlook.com>
2021-03-30 12:33:10 -0400

    Created router for updating data (updated planet details)

--- app.py
+++ app.py
@@ -206,6 +206,24 @@ def add_planet():
         db.session.commit()
         return jsonify(message="you added a planet")
 
+## Create route to update data
+@app.route('/update_planet', methods=['PUT'])
+@jwt_required()
+def update_planet():
+    planet_id = int(request.form['planet_id'])
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        planet.planet_name = request.form['planet_name']
+        planet.planet_type = request.form['planet_type']
+        planet.home_star = request.form['home_star']
+        planet.mass = float(request.form['mass'])
+        planet.radius = float(request.form['radius'])
+        planet.distance = float(request.form['distance'])
+        db.session.commit()
+        return jsonify(message="you updated a planet"), 202
+    else:
+        return jsonify(message="That planet doesn't exist")
+
 
 # database models
 class User(db.Model):
